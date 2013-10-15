751fc40351457ae29e9f2e7ed46e10f714189b4c
ccast2 <ccast2@gmail.com>
2013-10-14 22:23:27 -0500

    estilos arreglados

---
+++ server/application/logs/log-2013-10-15.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-10-15 04:51:52 --> Config Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> Hooks Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> Utf8 Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> UTF-8 Support Enabled
+DEBUG - 2013-10-15 04:51:52 --> URI Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> Router Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> Output Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> Security Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> Input Class Initialized
+DEBUG - 2013-10-15 04:51:52 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-10-15 04:51:53 --> Language Class Initialized
+DEBUG - 2013-10-15 04:51:53 --> Loader Class Initialized
+DEBUG - 2013-10-15 04:51:53 --> Database Driver Class Initialized
+ERROR - 2013-10-15 04:51:54 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\golombiao\server\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2013-10-15 04:51:54 --> Unable to connect to the database
+DEBUG - 2013-10-15 04:51:54 --> Language file loaded: language/english/db_lang.php
+DEBUG - 2013-10-15 04:51:55 --> Config Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Hooks Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Utf8 Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> UTF-8 Support Enabled
+DEBUG - 2013-10-15 04:51:55 --> URI Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Router Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Output Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Security Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Input Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-10-15 04:51:55 --> Language Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Loader Class Initialized
+DEBUG - 2013-10-15 04:51:55 --> Database Driver Class Initialized
+ERROR - 2013-10-15 04:51:56 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\golombiao\server\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2013-10-15 04:51:56 --> Unable to connect to the database
+DEBUG - 2013-10-15 04:51:56 --> Language file loaded: language/english/db_lang.php
+DEBUG - 2013-10-15 04:51:57 --> Config Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Hooks Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Utf8 Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> UTF-8 Support Enabled
+DEBUG - 2013-10-15 04:51:57 --> URI Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Router Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Output Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Security Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Input Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-10-15 04:51:57 --> Language Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Loader Class Initialized
+DEBUG - 2013-10-15 04:51:57 --> Database Driver Class Initialized
+ERROR - 2013-10-15 04:51:58 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\golombiao\server\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2013-10-15 04:51:58 --> Unable to connect to the database
+DEBUG - 2013-10-15 04:51:58 --> Language file loaded: language/english/db_lang.php
